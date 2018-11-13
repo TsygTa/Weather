a04a945397d3ddf696c6e6be9894ffc549a2f875
TsygTa <670724@gmail.com>
2018-11-14 00:09:55 +0300

    lesson-3-4

--- Weather/Weather/AllGroups/AllGroupsController.swift
+++ Weather/Weather/AllGroups/AllGroupsController.swift
@@ -8,17 +8,30 @@
 
 import UIKit
 
-class AllGroupsController: UITableViewController {
+class AllGroupsController: UITableViewController, UISearchBarDelegate {
     
     var groups = [
             DataModel(name: "Группа1", image: UIImage(named: "icon_1.png")!),
             DataModel(name: "Группа2", image: UIImage(named: "icon_3.png")!),
             DataModel(name: "Группа3", image: UIImage(named: "icon_4.png")!)
         ]
-
+    
+    var filteredGroups = [DataModel]()
+    
+    func groupFilter(_ searchText: String) -> [DataModel] {
+    
+        var result = [DataModel]()
+    
+        for group in groups {
+            if group.name.contains(searchText) {
+                result.append(group)
+            }
+        }
+        return result
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -26,6 +39,11 @@ class AllGroupsController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredGroups = groupFilter(searchText)
+    }
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -35,14 +53,24 @@ class AllGroupsController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return groups.count
+        if filteredGroups.count > 0 {
+            return filteredGroups.count
+        } else {
+            return groups.count
+        }
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "GroupCell", for: indexPath) as! AllGroupsCell
-
-        let group = groups[indexPath.row]
+        
+        let group: DataModel
+        
+        if filteredGroups.count > 0 {
+            group = filteredGroups[indexPath.row]
+        } else {
+             group = groups[indexPath.row]
+        }
         
         cell.groupName.text = group.name
         cell.groupAvatar.image = group.image
